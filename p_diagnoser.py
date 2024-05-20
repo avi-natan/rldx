270bcf25dc4e4ca0865c7a9b215f88672f12f440
avi-natan <avinat123@gmail.com>
2024-05-20 18:43:09 +0800

    added the SSP SSF

--- p_diagnoser.py
+++ p_diagnoser.py
@@ -722,6 +722,262 @@ def SD(env_name,
     return output
 
 
+def SSF(env_name,
+        render_mode,
+        ml_model_name,
+        total_timesteps,
+        fault_model,
+        lst_actions,
+        lst_states,
+        fault_probability,
+        instance_seed,
+        fault_models,
+        sample_size):
+    # welcome message
+    print(f'diagnosing with diagnoser: SSF\n========================================================================================')
+    # initialize environment
+    env = gym.make(env_name.replace('_', '-'), render_mode=render_mode)
+    initial_obs, _ = env.reset(seed=instance_seed)
+    # print(f'initial observation: {initial_obs.tolist()}')
+
+    # load trained model
+    models_dir = f"environments/{env_name}/models/{ml_model_name}"
+    model_path = f"{models_dir}/{total_timesteps}.zip"
+    model = models[ml_model_name].load(model_path, env=env)
+
+    # make sure the first state is the same in obs and simulation
+    s_0, _ = env.reset()
+    assert s_0.all() == lst_states[0].all()
+
+    # preparing the environments for each of the fault models
+    fault_model_trajectories = {}
+    for key in fault_models.keys():
+        fm_env = gym.make(env_name.replace('_', '-'), render_mode=render_mode)
+        fm_initial_obs, _ = fm_env.reset(seed=instance_seed)
+        fm_s_0, _ = fm_env.reset()
+        fault_model_trajectories[key] = [fault_models[key], fm_env, [fm_s_0], [], []]
+
+    total_time = 0.0
+    # running the diagnosis loop
+    for i in range(1, len(lst_states)):
+        # print(i)
+        ts0 = time.time()
+        a_gag_i, _ = model.predict(lst_states[i - 1], deterministic=DETERMINISTIC)
+        irrelevant_keys = []
+        te0 = time.time()
+        total_time += te0 - ts0
+        for fm_j, fm in enumerate(fault_model_trajectories.keys()):
+            ts1 = time.time()
+            # print(f'running {i}/{len(lst_states)}, fm {fm} ({fm_j}/{len(fault_model_trajectories.keys())})')
+            a_gag_ij = fault_model_trajectories[fm][0](a_gag_i)
+            te1 = time.time()
+            total_time += te1 - ts1
+
+            # reconstruct the states
+            a_gag_i_env = gym.make(env_name.replace('_', '-'), render_mode=render_mode)
+            a_gag_i_initial_obs, _ = a_gag_i_env.reset(seed=instance_seed)
+            a_gag_i_s_0, _ = a_gag_i_env.reset()
+            a_gag_ij_env = gym.make(env_name.replace('_', '-'), render_mode=render_mode)
+            a_gag_ij_initial_obs, _ = a_gag_ij_env.reset(seed=instance_seed)
+            a_gag_ij_s_0, _ = a_gag_ij_env.reset()
+            a_gag_i_s = a_gag_i_s_0
+            for i_rec in range(1, len(fault_model_trajectories[fm][2]), 2):
+                a = fault_model_trajectories[fm][2][i_rec]
+                a_gag_i_s, reward, done, trunc, info = a_gag_i_env.step(a)
+            a_gag_ij_s = a_gag_ij_s_0
+            for i_rec in range(1, len(fault_model_trajectories[fm][2]), 2):
+                a = fault_model_trajectories[fm][2][i_rec]
+                a_gag_ij_s, reward, done, trunc, info = a_gag_ij_env.step(a)
+
+            ts2 = time.time()
+            a_gag_i_s, reward, done, trunc, info = a_gag_i_env.step(a_gag_i)
+            a_gag_ij_s, reward, done, trunc, info = a_gag_ij_env.step(a_gag_ij)
+
+            if np.array_equal(a_gag_i_s, lst_states[i]) and np.array_equal(a_gag_ij_s, lst_states[i]):
+                fault_model_trajectories[fm][2].append(a_gag_i)
+                fault_model_trajectories[fm][2].append(a_gag_i_s)
+                fault_model_trajectories[fm][3].append(1)
+            elif np.array_equal(a_gag_i_s, lst_states[i]) and not np.array_equal(a_gag_ij_s, lst_states[i]):
+                fault_model_trajectories[fm][2].append(a_gag_i)
+                fault_model_trajectories[fm][2].append(a_gag_i_s)
+                fault_model_trajectories[fm][3].append(1 - fault_probability)
+            elif not np.array_equal(a_gag_i_s, lst_states[i]) and not np.array_equal(a_gag_ij_s, lst_states[i]):
+                irrelevant_keys.append(fm)
+            elif not np.array_equal(a_gag_i_s, lst_states[i]) and np.array_equal(a_gag_ij_s, lst_states[i]):
+                fault_model_trajectories[fm][2].append(a_gag_ij)
+                fault_model_trajectories[fm][2].append(a_gag_ij_s)
+                fault_model_trajectories[fm][3].append(fault_probability)
+                fault_model_trajectories[fm][4].append(i)
+            te2 = time.time()
+            total_time += te2 - ts2
+
+        ts3 = time.time()
+        for fm in irrelevant_keys:
+            fault_model_trajectories.pop(fm)
+        te3 = time.time()
+        total_time += te3 - ts3
+        if len(fault_model_trajectories) == 1:
+            # print(f"i broke at {i}")
+            break
+
+    # finilizing the output
+    ts4 = time.time()
+    determined_fms = []
+    for key in fault_model_trajectories.keys():
+        determined_fms.append(key)
+    te4 = time.time()
+    total_time += te4 - ts4
+
+    # give ranking if there are more than one fault model surviving
+    ts5 = time.time()
+    if len(determined_fms) > 1:
+        fms_ranks = []
+        for fm_i, fm in enumerate(determined_fms):
+            lst_actions_fm = []
+            for i in range(1, len(fault_model_trajectories[fm][2]), 2):
+                lst_actions_fm.append(fault_model_trajectories[fm][2][i])
+            num_actual_faults = 0
+            for i in range(len(lst_actions_fm)):
+                if lst_actions[i] != lst_actions_fm[i]:
+                    num_actual_faults += 1
+            num_potential_faults = 0
+            for i in range(len(lst_actions_fm)):
+                a = lst_actions[i]
+                fa = fault_model_trajectories[fm][0](a)
+                if a != fa:
+                    num_potential_faults += 1
+            rank = num_actual_faults * 1.0 / num_potential_faults
+            fms_ranks.append(rank)
+        zipped_lists = zip(determined_fms, fms_ranks)
+        sorted_zipped_lists = sorted(zipped_lists, key=lambda x: x[-1], reverse=True)
+        determined_fms, fms_ranks = zip(*sorted_zipped_lists)
+
+    te5 = time.time()
+    total_time += te5 - ts5
+    elapsed_time_sec = total_time
+    elapsed_time_ms = elapsed_time_sec * 1000
+
+    try:
+        fm_rank = determined_fms.index(fault_model)
+    except ValueError:
+        fm_rank = len(fault_models)
+
+    output = {
+        "fm": determined_fms,
+        # "traj": determined_trajectories,
+        # "fp": determined_fault_probabilities,
+        # "fp_prod": determined_fault_probabilities_products,
+        # "pof": determined_points_of_failure,
+        "fm_rank": fm_rank,
+        "runtime_sec": elapsed_time_sec,
+        "runtime_ms": elapsed_time_ms
+    }
+    return output
+
+
+def SSP(env_name,
+        render_mode,
+        ml_model_name,
+        total_timesteps,
+        fault_model,
+        lst_actions,
+        lst_states,
+        fault_probability,
+        instance_seed,
+        fault_models,
+        sample_size):
+    # welcome message
+    print(f'diagnosing with diagnoser: SSP\n========================================================================================')
+    # initialize environment
+    env = gym.make(env_name.replace('_', '-'), render_mode=render_mode)
+    initial_obs, _ = env.reset(seed=instance_seed)
+    # print(f'initial observation: {initial_obs.tolist()}')
+
+    # load trained model
+    models_dir = f"environments/{env_name}/models/{ml_model_name}"
+    model_path = f"{models_dir}/{total_timesteps}.zip"
+    model = models[ml_model_name].load(model_path, env=env)
+
+    # make sure the first state is the same in obs and simulation
+    s_0, _ = env.reset()
+    assert s_0.all() == lst_states[0].all()
+
+    # preparing the environments for each of the fault models
+    fault_model_trajectories = {}
+    for key in fault_models.keys():
+        fm_env = gym.make(env_name.replace('_', '-'), render_mode=render_mode)
+        fm_initial_obs, _ = fm_env.reset(seed=instance_seed)
+        fm_s_0, _ = fm_env.reset()
+        fault_model_trajectories[key] = [fault_models[key], fm_env, [fm_s_0], []]
+
+    start_time = time.time()
+    # running the diagnosis loop
+    Tj_gags = {}
+    for fm_j, fm in enumerate(fault_models.keys()):
+        Tj_gag = []
+        for i in range(sample_size):
+            # print(f'{fm_j}/{i}')
+            fm_env = gym.make(env_name.replace('_', '-'), render_mode=render_mode)
+            fm_initial_obs, _ = fm_env.reset(seed=instance_seed)
+            fm_s_0, _ = fm_env.reset()
+            t_j_i = [fm_s_0]
+            S_r_minus_1 = fm_s_0
+            for r in range(1, len(lst_states)):
+                if random.random() < fault_probability:
+                    a_r, _ = model.predict(S_r_minus_1, deterministic=DETERMINISTIC)
+                    a_r = fault_models[fm](a_r)
+                else:
+                    a_r, _ = model.predict(S_r_minus_1, deterministic=DETERMINISTIC)
+                t_j_i.append(a_r)
+                S_r, reward, done, trunc, info = fm_env.step(a_r)
+                t_j_i.append(S_r)
+                S_r_minus_1 = S_r
+            Tj_gag.append(t_j_i)
+        Tj_gags[fm] = Tj_gag
+    simis = {}
+    lst_states_flat = [s.flatten() if s is not None else None for s in lst_states]
+    for fm_j, fm in enumerate(fault_models.keys()):
+        simis_j = []
+        for i in range(sample_size):
+            t_j_i_states = [s for s_i, s in enumerate(Tj_gags[fm][i]) if s_i % 2 == 0]
+            t_j_i_states_flat = [s.flatten() for s in t_j_i_states]
+            # similarity
+            simi_j_i = 0.0
+            for r in range(len(t_j_i_states_flat)):
+                if t_j_i_states_flat[r] is not None and lst_states_flat[r] is not None:
+                    dot_st = sum(a * b for a, b in zip(lst_states_flat[r], t_j_i_states_flat[r]))
+                    dot_ss = sum(a * b for a, b in zip(lst_states_flat[r], lst_states_flat[r]))
+                    dot_tt = sum(a * b for a, b in zip(t_j_i_states_flat[r], t_j_i_states_flat[r]))
+                    simi_j_i_r = dot_st / ((dot_ss ** .5) * (dot_tt ** .5))
+                    simi_j_i += simi_j_i_r
+            simis_j.append(simi_j_i)
+        simis[fm] = simis_j
+    scores = {}
+    for fm_j, fm in enumerate(fault_models.keys()):
+        score_j = sum(simis[fm]) / len(simis[fm])
+        scores[fm] = score_j
+    end_time = time.time()
+    elapsed_time_sec = end_time - start_time
+    elapsed_time_ms = elapsed_time_sec * 1000
+
+    # finilizing the output
+    sorted_zipped_lists = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+    determined_fms, ranks = zip(*sorted_zipped_lists)
+
+    try:
+        fm_rank = determined_fms.index(fault_model)
+    except ValueError:
+        fm_rank = len(determined_fms)
+    output = {
+        "fm": determined_fms,
+        "fm_rank": fm_rank,
+        "runtime_sec": elapsed_time_sec,
+        "runtime_ms": elapsed_time_ms
+    }
+
+    return output
+
+
 diagnosers = {
     # abstract fault models (suitable for all environments)
     "DWF": DWF,
@@ -731,5 +987,7 @@ diagnosers = {
     "SSF_SMP": SSF_SMP,
     "SSF_PF": SSF_PF,
     "W": W,
-    "SD": SD
+    "SD": SD,
+    "SSF": SSF,
+    "SSP": SSP
 }
