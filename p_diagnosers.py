229bdd86395bedc90f1eaa46a301b29b9e5e4fc5
avi-natan <avinat123@gmail.com>
2024-07-18 09:29:38 +0800

    organizing the time recording before adding overhead time recording

--- p_diagnosers.py
+++ p_diagnosers.py
@@ -22,7 +22,10 @@ def W(debug_print, render_mode, instance_seed, ml_model_name, domain_name, obser
     S_0, _ = simulator.reset()
     assert comparators[domain_name](observations[0], S_0)
 
-    diagnosis_start_time = time.time()
+    # initialize time counting
+    diagnosis_runtime_sec = 0.0
+
+    ts0 = time.time()
     b = 0
     e = len(observations) - 1
     S = observations[0]
@@ -41,8 +44,10 @@ def W(debug_print, render_mode, instance_seed, ml_model_name, domain_name, obser
     D = []
     for i in range(b + 1, e + 1):
         D.append(i)
-    diagnosis_end_time = time.time()
-    diagnosis_runtime_sec = diagnosis_end_time - diagnosis_start_time
+    te0 = time.time()
+    diagnosis_runtime_sec += te0 - ts0
+
+    # finilizing the runtime in ms
     diagnosis_runtime_ms = diagnosis_runtime_sec * 1000
 
     output = {
@@ -80,10 +85,10 @@ def SN(debug_print, render_mode, instance_seed, ml_model_name, domain_name, obse
     diagnosis_runtime_sec += te0 - ts0
 
     # running the diagnosis loop
+    ts1 = time.time()
     for i in range(1, len(observations)):
         irrelevant_keys = []
         for key_j in G.keys():
-            ts1 = time.time()
             a_gag_i, _ = policy.predict(refiners[domain_name](G[key_j][2]), deterministic=DETERMINISTIC)
             a_gag_i_j = G[key_j][0](a_gag_i)
             simulator.set_state(G[key_j][2])
@@ -93,15 +98,10 @@ def SN(debug_print, render_mode, instance_seed, ml_model_name, domain_name, obse
             if observations[i] is not None:
                 if not comparators[domain_name](observations[i], S_gag_i_j):
                     irrelevant_keys.append(key_j)
-            te1 = time.time()
-            diagnosis_runtime_sec += te1 - ts1
 
         # remove the irrelevant fault modes
-        ts2 = time.time()
         for key in irrelevant_keys:
             G.pop(key)
-        te2 = time.time()
-        diagnosis_runtime_sec += te2 - ts2
 
         if debug_print:
             print(f'STEP {i}/{len(observations)}: KICKED {len(irrelevant_keys)} ({len(G)}) at time {diagnosis_runtime_sec}: {str(irrelevant_keys)}')
@@ -110,6 +110,8 @@ def SN(debug_print, render_mode, instance_seed, ml_model_name, domain_name, obse
             if debug_print:
                 print(f"i broke at {i}")
             break
+    te1 = time.time()
+    diagnosis_runtime_sec += te1 - ts1
 
     # finilizing the runtime in ms
     diagnosis_runtime_ms = diagnosis_runtime_sec * 1000
@@ -167,18 +169,15 @@ def SIF(debug_print, render_mode, instance_seed, ml_model_name, domain_name, obs
     diagnosis_runtime_sec += te0 - ts0
 
     for i in range(1, len(observations)):
+        ts1 = time.time()
         irrelevant_keys = []
         new_relevant_keys = {}
         for key_j in G.keys():
-            ts1 = time.time()
             a_gag_i, _ = policy.predict(refiners[domain_name](G[key_j][2]), deterministic=DETERMINISTIC)
             a_gag_i = int(a_gag_i)
             a_gag_i_j = G[key_j][0](a_gag_i)
-            te1 = time.time()
-            diagnosis_runtime_sec += te1 - ts1
 
             # apply the normal and the faulty action on the reconstructed states, respectively
-            ts2 = time.time()
             simulator.set_state(G[key_j][2])
             S_gag_i, reward, done, trunc, info = simulator.step(a_gag_i)
             simulator.set_state(G[key_j][2])
@@ -225,18 +224,15 @@ def SIF(debug_print, render_mode, instance_seed, ml_model_name, domain_name, obs
                     k_j = key_j.split('_')[0]
                     new_relevant_keys[k_j + f'_{I[k_j]}'] = [candidate_fault_modes[k_j],  A_j_to_fault, S_gag_i_j]
                     I[k_j] = I[k_j] + 1
-            te2 = time.time()
-            diagnosis_runtime_sec += te2 - ts2
-
         # add new relevant fault modes
-        ts3 = time.time()
         for key in new_relevant_keys:
             G[key] = new_relevant_keys[key]
         # remove the irrelevant fault modes
         for key in irrelevant_keys:
             G.pop(key)
-        te3 = time.time()
-        diagnosis_runtime_sec += te3 - ts3
+        te1 = time.time()
+        diagnosis_runtime_sec += te1 - ts1
+
         # filter out similar trajectories (applies to taxi only)
         if domain_name == "Taxi_v3":
             FG = {}
@@ -292,23 +288,6 @@ def SIFU(debug_print, render_mode, instance_seed, ml_model_name, domain_name, ob
     # initialize time counting
     diagnosis_runtime_sec = 0.0
 
-    # compute index queue (the computed is of the form: [(b1,e1), (b2,e2), ..., (bm,em)]  )
-    ts = time.time()
-    index_pairs = {}
-    i = 0
-    for j in range(1, len(observations)):
-        if observations[j] is None:
-            continue
-        else:
-            i_s = str(i).zfill(3)
-            j_s = str(j).zfill(3)
-            index_pairs[f"{i_s}_{j_s}"] = j - i
-            i = j
-    sorted_index_pairs = sorted(index_pairs.keys(), key=lambda k: (index_pairs[k], k))
-    index_queue = [(int(item.split("_")[0]), int(item.split("_")[1])) for item in sorted_index_pairs]
-    te = time.time()
-    diagnosis_runtime_sec += te - ts
-
     # initialize maximum size of G
     G_max_size = 0
 
@@ -326,24 +305,38 @@ def SIFU(debug_print, render_mode, instance_seed, ml_model_name, domain_name, ob
     te0 = time.time()
     diagnosis_runtime_sec += te0 - ts0
 
+    # compute index queue (the computed is of the form: [(b1,e1), (b2,e2), ..., (bm,em)]  )
+    ts1 = time.time()
+    index_pairs = {}
+    i = 0
+    for j in range(1, len(observations)):
+        if observations[j] is None:
+            continue
+        else:
+            i_s = str(i).zfill(3)
+            j_s = str(j).zfill(3)
+            index_pairs[f"{i_s}_{j_s}"] = j - i
+            i = j
+    sorted_index_pairs = sorted(index_pairs.keys(), key=lambda k: (index_pairs[k], k))
+    index_queue = [(int(item.split("_")[0]), int(item.split("_")[1])) for item in sorted_index_pairs]
+    te1 = time.time()
+    diagnosis_runtime_sec += te1 - ts1
+
     for irk in index_queue:
         if len(G) == 1:
             break
         for key in G.keys():
             G[key][2] = observations[irk[0]]
         for i in range(irk[0]+1, irk[1]+1):
+            ts2 = time.time()
             irrelevant_keys = []
             new_relevant_keys = {}
             for key_j in G.keys():
-                ts1 = time.time()
                 a_gag_i, _ = policy.predict(refiners[domain_name](G[key_j][2]), deterministic=DETERMINISTIC)
                 a_gag_i = int(a_gag_i)
                 a_gag_i_j = G[key_j][0](a_gag_i)
-                te1 = time.time()
-                diagnosis_runtime_sec += te1 - ts1
 
                 # apply the normal and the faulty action on the reconstructed states, respectively
-                ts2 = time.time()
                 simulator.set_state(G[key_j][2])
                 S_gag_i, reward, done, trunc, info = simulator.step(a_gag_i)
                 simulator.set_state(G[key_j][2])
@@ -390,18 +383,15 @@ def SIFU(debug_print, render_mode, instance_seed, ml_model_name, domain_name, ob
                         k_j = key_j.split('_')[0]
                         new_relevant_keys[k_j + f'_{I[k_j]}'] = [candidate_fault_modes[k_j],  A_j_to_fault, S_gag_i_j]
                         I[k_j] = I[k_j] + 1
-                te2 = time.time()
-                diagnosis_runtime_sec += te2 - ts2
-
             # add new relevant fault modes
-            ts3 = time.time()
             for key in new_relevant_keys:
                 G[key] = new_relevant_keys[key]
             # remove the irrelevant fault modes
             for key in irrelevant_keys:
                 G.pop(key)
-            te3 = time.time()
-            diagnosis_runtime_sec += te3 - ts3
+            te2 = time.time()
+            diagnosis_runtime_sec += te2 - ts2
+
             # filter out similar trajectories (applies to taxi only)
             if domain_name == "Taxi_v3":
                 FG = {}
@@ -457,8 +447,25 @@ def SIFU2(debug_print, render_mode, instance_seed, ml_model_name, domain_name, o
     # initialize time counting
     diagnosis_runtime_sec = 0.0
 
+    # initialize maximum size of G
+    G_max_size = 0
+
+    # initialize unique ID's for each fault mode in order to represent different branchings
+    I = {}
+    for key_j in candidate_fault_modes:
+        I[key_j] = 0
+
+    # initialize G
+    ts0 = time.time()
+    G = {}
+    for key_j in candidate_fault_modes:
+        G[key_j + f'_{I[key_j]}'] = [candidate_fault_modes[key_j], [None] * (len(observations)-1), None]
+        I[key_j] = I[key_j] + 1
+    te0 = time.time()
+    diagnosis_runtime_sec += te0 - ts0
+
     # compute index queue (the computed is of the form: [(b1,e1), (b2,e2), ..., (bm,em)]  )
-    ts = time.time()
+    ts1 = time.time()
     index_pairs = {}
     i = 0
     for j in range(1, len(observations)):
@@ -483,25 +490,8 @@ def SIFU2(debug_print, render_mode, instance_seed, ml_model_name, domain_name, o
             useful_index_pairs[pair] = index_pairs[pair]
     sorted_useful_index_pairs = sorted(useful_index_pairs.keys(), key=lambda k: (useful_index_pairs[k], k))
     index_queue = [(int(item.split("_")[0]), int(item.split("_")[1])) for item in sorted_useful_index_pairs]
-    te = time.time()
-    diagnosis_runtime_sec += te - ts
-
-    # initialize maximum size of G
-    G_max_size = 0
-
-    # initialize unique ID's for each fault mode in order to represent different branchings
-    I = {}
-    for key_j in candidate_fault_modes:
-        I[key_j] = 0
-
-    # initialize G
-    ts0 = time.time()
-    G = {}
-    for key_j in candidate_fault_modes:
-        G[key_j + f'_{I[key_j]}'] = [candidate_fault_modes[key_j], [None] * (len(observations)-1), None]
-        I[key_j] = I[key_j] + 1
-    te0 = time.time()
-    diagnosis_runtime_sec += te0 - ts0
+    te1 = time.time()
+    diagnosis_runtime_sec += te1 - ts1
 
     for irk in index_queue:
         if len(G) == 1:
@@ -509,18 +499,15 @@ def SIFU2(debug_print, render_mode, instance_seed, ml_model_name, domain_name, o
         for key in G.keys():
             G[key][2] = observations[irk[0]]
         for i in range(irk[0]+1, irk[1]+1):
+            ts2 = time.time()
             irrelevant_keys = []
             new_relevant_keys = {}
             for key_j in G.keys():
-                ts1 = time.time()
                 a_gag_i, _ = policy.predict(refiners[domain_name](G[key_j][2]), deterministic=DETERMINISTIC)
                 a_gag_i = int(a_gag_i)
                 a_gag_i_j = G[key_j][0](a_gag_i)
-                te1 = time.time()
-                diagnosis_runtime_sec += te1 - ts1
 
                 # apply the normal and the faulty action on the reconstructed states, respectively
-                ts2 = time.time()
                 simulator.set_state(G[key_j][2])
                 S_gag_i, reward, done, trunc, info = simulator.step(a_gag_i)
                 simulator.set_state(G[key_j][2])
@@ -567,18 +554,15 @@ def SIFU2(debug_print, render_mode, instance_seed, ml_model_name, domain_name, o
                         k_j = key_j.split('_')[0]
                         new_relevant_keys[k_j + f'_{I[k_j]}'] = [candidate_fault_modes[k_j],  A_j_to_fault, S_gag_i_j]
                         I[k_j] = I[k_j] + 1
-                te2 = time.time()
-                diagnosis_runtime_sec += te2 - ts2
-
             # add new relevant fault modes
-            ts3 = time.time()
             for key in new_relevant_keys:
                 G[key] = new_relevant_keys[key]
             # remove the irrelevant fault modes
             for key in irrelevant_keys:
                 G.pop(key)
-            te3 = time.time()
-            diagnosis_runtime_sec += te3 - ts3
+            te2 = time.time()
+            diagnosis_runtime_sec += te2 - ts2
+
             # filter out similar trajectories (applies to taxi only)
             if domain_name == "Taxi_v3":
                 FG = {}
@@ -634,61 +618,6 @@ def SIFU3(debug_print, render_mode, instance_seed, ml_model_name, domain_name, o
     # initialize time counting
     diagnosis_runtime_sec = 0.0
 
-    # compute index queue (the computed is of the form: [(b1,e1), (b2,e2), ..., (bm,em)]  )
-    # at the same time, collect the action types to be tested
-    ts = time.time()
-    index_pairs = {}
-    i = 0
-    for j in range(1, len(observations)):
-        if observations[j] is None:
-            continue
-        else:
-            i_s = str(i).zfill(3)
-            j_s = str(j).zfill(3)
-            index_pairs[f"{i_s}_{j_s}"] = [j - i, None]
-            i = j
-
-    index_pairs_failed = {}
-    for pair in index_pairs:
-        b = int(pair.split("_")[0])
-        e = int(pair.split("_")[1])
-        S = observations[b]
-        simulator.set_state(S)
-        for i in range(e - b):
-            a, _ = policy.predict(refiners[domain_name](S), deterministic=DETERMINISTIC)
-            a = int(a)
-            # print(f'i {b + i}: a {a}')
-            S, reward, done, trunc, info = simulator.step(a)
-        if not comparators[domain_name](observations[e], S):
-            index_pairs_failed[pair] = [index_pairs[pair][0], set()]
-            # index_pairs[pair][1] = 'FAIL'
-            # print(f'pair {pair}: FAIL\n')
-        # else:
-        #     index_pairs[pair][1] = '  OK'
-            # print(f'pair {pair}: OK\n')
-    index_pairs_failed_sorted = {k: v for k, v in sorted(index_pairs_failed.items(), key=lambda item: (item[1][0], -len(item[1][1]), item[0]))}
-
-    index_pairs_failed_sorted_useful = {}
-    action_types_combined = set()
-    for pair in index_pairs_failed_sorted:
-        b = int(pair.split("_")[0])
-        e = int(pair.split("_")[1])
-        S = observations[b]
-        simulator.set_state(S)
-        for i in range(e - b):
-            a, _ = policy.predict(refiners[domain_name](S), deterministic=DETERMINISTIC)
-            a = int(a)
-            # print(f'i {b+i}: a {a}')
-            index_pairs_failed_sorted[pair][1].add(a)
-            S, reward, done, trunc, info = simulator.step(a)
-        if len(index_pairs_failed_sorted[pair][1].difference(action_types_combined)) != 0:
-            index_pairs_failed_sorted_useful[pair] = [index_pairs_failed_sorted[pair][0], index_pairs_failed_sorted[pair][1]]
-            action_types_combined.update(index_pairs_failed_sorted[pair][1])
-
-    index_queue = [(int(item.split("_")[0]), int(item.split("_")[1])) for item in index_pairs_failed_sorted_useful.keys()]
-    te = time.time()
-    diagnosis_runtime_sec += te - ts
-
     # initialize maximum size of G
     G_max_size = 0
 
@@ -706,24 +635,73 @@ def SIFU3(debug_print, render_mode, instance_seed, ml_model_name, domain_name, o
     te0 = time.time()
     diagnosis_runtime_sec += te0 - ts0
 
+    # compute index queue (the computed is of the form: [(b1,e1), (b2,e2), ..., (bm,em)]  )
+    # at the same time, collect the action types to be tested
+    ts1 = time.time()
+    index_pairs = {}
+    i = 0
+    for j in range(1, len(observations)):
+        if observations[j] is None:
+            continue
+        else:
+            i_s = str(i).zfill(3)
+            j_s = str(j).zfill(3)
+            index_pairs[f"{i_s}_{j_s}"] = [j - i, None]
+            i = j
+    index_pairs_failed = {}
+    for pair in index_pairs:
+        b = int(pair.split("_")[0])
+        e = int(pair.split("_")[1])
+        S = observations[b]
+        simulator.set_state(S)
+        for i in range(e - b):
+            a, _ = policy.predict(refiners[domain_name](S), deterministic=DETERMINISTIC)
+            a = int(a)
+            # print(f'i {b + i}: a {a}')
+            S, reward, done, trunc, info = simulator.step(a)
+        if not comparators[domain_name](observations[e], S):
+            index_pairs_failed[pair] = [index_pairs[pair][0], set()]
+            # index_pairs[pair][1] = 'FAIL'
+            # print(f'pair {pair}: FAIL\n')
+        # else:
+        #     index_pairs[pair][1] = '  OK'
+        # print(f'pair {pair}: OK\n')
+    index_pairs_failed_sorted = {k: v for k, v in sorted(index_pairs_failed.items(), key=lambda item: (item[1][0], -len(item[1][1]), item[0]))}
+    index_pairs_failed_sorted_useful = {}
+    action_types_combined = set()
+    for pair in index_pairs_failed_sorted:
+        b = int(pair.split("_")[0])
+        e = int(pair.split("_")[1])
+        S = observations[b]
+        simulator.set_state(S)
+        for i in range(e - b):
+            a, _ = policy.predict(refiners[domain_name](S), deterministic=DETERMINISTIC)
+            a = int(a)
+            # print(f'i {b+i}: a {a}')
+            index_pairs_failed_sorted[pair][1].add(a)
+            S, reward, done, trunc, info = simulator.step(a)
+        if len(index_pairs_failed_sorted[pair][1].difference(action_types_combined)) != 0:
+            index_pairs_failed_sorted_useful[pair] = [index_pairs_failed_sorted[pair][0], index_pairs_failed_sorted[pair][1]]
+            action_types_combined.update(index_pairs_failed_sorted[pair][1])
+    index_queue = [(int(item.split("_")[0]), int(item.split("_")[1])) for item in index_pairs_failed_sorted_useful.keys()]
+    te1 = time.time()
+    diagnosis_runtime_sec += te1 - ts1
+
     for irk in index_queue:
         if len(G) == 1:
             break
         for key in G.keys():
             G[key][2] = observations[irk[0]]
         for i in range(irk[0]+1, irk[1]+1):
+            ts2 = time.time()
             irrelevant_keys = []
             new_relevant_keys = {}
             for key_j in G.keys():
-                ts1 = time.time()
                 a_gag_i, _ = policy.predict(refiners[domain_name](G[key_j][2]), deterministic=DETERMINISTIC)
                 a_gag_i = int(a_gag_i)
                 a_gag_i_j = G[key_j][0](a_gag_i)
-                te1 = time.time()
-                diagnosis_runtime_sec += te1 - ts1
 
                 # apply the normal and the faulty action on the reconstructed states, respectively
-                ts2 = time.time()
                 simulator.set_state(G[key_j][2])
                 S_gag_i, reward, done, trunc, info = simulator.step(a_gag_i)
                 simulator.set_state(G[key_j][2])
@@ -770,18 +748,15 @@ def SIFU3(debug_print, render_mode, instance_seed, ml_model_name, domain_name, o
                         k_j = key_j.split('_')[0]
                         new_relevant_keys[k_j + f'_{I[k_j]}'] = [candidate_fault_modes[k_j],  A_j_to_fault, S_gag_i_j]
                         I[k_j] = I[k_j] + 1
-                te2 = time.time()
-                diagnosis_runtime_sec += te2 - ts2
-
             # add new relevant fault modes
-            ts3 = time.time()
             for key in new_relevant_keys:
                 G[key] = new_relevant_keys[key]
             # remove the irrelevant fault modes
             for key in irrelevant_keys:
                 G.pop(key)
-            te3 = time.time()
-            diagnosis_runtime_sec += te3 - ts3
+            te2 = time.time()
+            diagnosis_runtime_sec += te2 - ts2
+
             # filter out similar trajectories (applies to taxi only)
             if domain_name == "Taxi_v3":
                 FG = {}
